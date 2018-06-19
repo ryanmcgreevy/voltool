2197b6880f66451429fe52b08da0e703ad619522
Ryan McGreevy <ryanmcgreevy@gmail.com>
2018-06-19 16:02:42 -0400

    simplified binary add

--- Voltool.C
+++ Voltool.C
@@ -701,52 +701,26 @@ void add(VolumetricData *mapA, VolumetricData  *mapB, VolumetricData *newvol, bo
   // allows for precisely subtracting unaligned maps, and/or maps of
   // different resolutions
 
-//  if (flagsbits & USE_INTERP) interp = true;
-
   if ( USE_UNION) {
-
     // UNION VERSION
-      
     init_from_union(mapA, mapB, newvol);
-
-    // use a 'safe' version of voxel_value_interpolate_from_coord
-    // that returns zero if coordinate is outside the map
-
-    for (gx=0; gx<newvol->xsize; gx++)
-      for (gy=0; gy<newvol->ysize; gy++)
-        for (gz=0; gz<newvol->zsize; gz++) {
-          float x, y, z;
-          voxel_coord(gx, gy, gz, x, y, z, newvol);
-
-          if (interp) newvol->data[gz*newvol->xsize*newvol->ysize + gy*newvol->xsize + gx] = \
-            mapA->voxel_value_interpolate_from_coord(x,y,z) + \
-            mapB->voxel_value_interpolate_from_coord(x,y,z);
-          else newvol->data[gz*newvol->xsize*newvol->ysize + gy*newvol->xsize + gx] = \
-            mapA->voxel_value_from_coord(x,y,z) + \
-            mapB->voxel_value_from_coord(x,y,z);
-        }
-
   } else {
-  
     // INTERSECTION VERSION
-    
-    
     init_from_intersection(mapA, mapB, newvol);
-  
-    for (gx=0; gx<newvol->xsize; gx++)
-      for (gy=0; gy<newvol->ysize; gy++)
-        for (gz=0; gz<newvol->zsize; gz++) {
-          float x, y, z;
-          voxel_coord(gx, gy, gz, x, y, z, newvol);
-          if (interp) newvol->data[gz*newvol->xsize*newvol->ysize + gy*newvol->xsize + gx] = \
-            mapA->voxel_value_interpolate_from_coord(x,y,z) + \
-            mapB->voxel_value_interpolate_from_coord(x,y,z);
-          else newvol->data[gz*newvol->xsize*newvol->ysize + gy*newvol->xsize + gx] = \
-            mapA->voxel_value_from_coord(x,y,z) + \
-            mapB->voxel_value_from_coord(x,y,z);
-        }
-
   }
+  for (gx=0; gx<newvol->xsize; gx++)
+    for (gy=0; gy<newvol->ysize; gy++)
+      for (gz=0; gz<newvol->zsize; gz++) {
+        float x, y, z;
+        voxel_coord(gx, gy, gz, x, y, z, newvol);
+
+        if (interp) newvol->data[gz*newvol->xsize*newvol->ysize + gy*newvol->xsize + gx] = \
+          mapA->voxel_value_interpolate_from_coord(x,y,z) + \
+          mapB->voxel_value_interpolate_from_coord(x,y,z);
+        else newvol->data[gz*newvol->xsize*newvol->ysize + gy*newvol->xsize + gx] = \
+          mapA->voxel_value_from_coord(x,y,z) + \
+          mapB->voxel_value_from_coord(x,y,z);
+      }
 
 }
 
